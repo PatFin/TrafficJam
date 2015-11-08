4e0bbdb111176cda4a4dfc513cbc2e475fb6f470
patrick <patrick.finnerty@insa-lyon.fr>
2015-11-08 16:29:28 +0000

    Zope.

--- TrafficJamProject/Debug/subdir.mk
+++ TrafficJamProject/Debug/subdir.mk
@@ -4,31 +4,31 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../Begin.cpp \
 ../City.cpp \
 ../Event.cpp \
 ../EventList.cpp \
 ../Sensor.cpp \
 ../SensorLeaf.cpp \
-../SensorRoot.cpp \
-../TestAlexandre.cpp 
+../SensorRoot.cpp 
 
 OBJS += \
+./Begin.o \
 ./City.o \
 ./Event.o \
 ./EventList.o \
 ./Sensor.o \
 ./SensorLeaf.o \
-./SensorRoot.o \
-./TestAlexandre.o 
+./SensorRoot.o 
 
 CPP_DEPS += \
+./Begin.d \
 ./City.d \
 ./Event.d \
 ./EventList.d \
 ./Sensor.d \
 ./SensorLeaf.d \
-./SensorRoot.d \
-./TestAlexandre.d 
+./SensorRoot.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
